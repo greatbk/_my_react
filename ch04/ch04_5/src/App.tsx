672520161da0e3e10a5f5ec5c68b87c33970df7a
gangil <greatbk@daum.net>
2023-04-02 19:10:37 +0900

    Initial commit

---
+++ ch04/ch04_5/src/App.tsx
@@ -0,0 +1,19 @@
+import ValidatableInputTest from './pages/ValidatableInputTest'
+import ForwardRefTest from './pages/ForwardRefTest'
+import InputValueTest from './pages/InputValueTest'
+import InputFocusTest from './pages/InputFocusTest'
+import FileDrop from './pages/FileDrop'
+import ClickTest from './pages/ClickTest'
+
+export default function App() {
+  return (
+    <main>
+      <ValidatableInputTest />
+      <ForwardRefTest />
+      <InputValueTest />
+      <InputFocusTest />
+      <FileDrop />
+      <ClickTest />
+    </main>
+  )
+}
