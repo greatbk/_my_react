d49a556da6909811e446489f9b084a3cdd663907
gangil <greatbk@daum.net>
2023-04-04 18:49:50 +0900

    .

--- ch05/ch05_4/src/store/cardEntities/reducers.ts
+++ ch05/ch05_4/src/store/cardEntities/reducers.ts
@@ -6,10 +6,11 @@ export const reducer = (state: T.State = initialState, action: T.Actions) => {
   switch (action.type) {
     case '@cardEntities/add':
       return {...state, [action.payload.uuid]: action.payload}
-    case '@cardEntities/remove':
+    case '@cardEntities/remove': {
       const newState = {...state}
       delete newState[action.payload]
       return newState
+    }
   }
   return state
 }
--- ch05/ch05_4/src/store/useStore.ts
+++ ch05/ch05_4/src/store/useStore.ts
@@ -1,9 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {useMemo} from 'react'
 import {rootReducer} from './rootReducer'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializeStore = () => {
-  return configureStore({reducer: rootReducer, middleware: []})
+  const middleware: any[] = [thunk]
+  if (useLogger) {
+    middleware.push(logger)
+  }
+
+  const store = configureStore({reducer: rootReducer, middleware})
+  return store
 }
 
 export function useStore() {
